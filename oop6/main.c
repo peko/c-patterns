33ecfbb47bc3c495a9504c7a210f2807b3023219
peko <gasu.basu@gmail.com>
2018-06-14 13:04:50 +0300

    up

--- oop6/main.c
+++ oop6/main.c
@@ -32,12 +32,14 @@ typedef struct cat {
 	animal animal;
 } cat;
 
-void method_voice_cat(animal* a) {
+static void 
+method_voice_cat(animal* a) {
     cat* c = container_of(a, cat, animal);
-	printf("%s says: Meow\n", c->name);
+	printf("%s the cat says: \"Meow\"\n", c->name);
 }
 
-void method_free_cat(animal* a) {
+static void 
+method_free_cat(animal* a) {
     cat* c = container_of(a, cat, animal);
     free(c->name);
 	free(c);
@@ -62,28 +64,74 @@ typedef struct dog {
 	animal animal;
 } dog;
 
-void method_voice_dog(animal* a) {
+static void 
+method_voice_dog(animal* a) {
     dog* c = container_of(a, dog, animal);
-	printf("%s says: Bark\n", c->name);
+	printf("%s the dog says: \"Bark\"\n", c->name);
 }
 
-void method_free_dog(animal* a) {
+static void 
+method_free_dog(animal* a) {
     dog* d = container_of(a, dog, animal);
     free(d->name);
 	free(d);
 }
 
-struct animal_vtable animal_dog_vtable = {
+struct 
+animal_vtable animal_dog_vtable = {
 	method_voice_dog, 
 	method_free_dog
 };
 
-animal* animal_dog_create(const char* name) {
+animal* 
+animal_dog_create(const char* name) {
 	struct dog* d = malloc(sizeof(*d));
 	d->animal.vtable = &animal_dog_vtable;
 	d->name = strdup(name);
 	return &d->animal;
 }
+
+// catdog composition
+
+typedef struct catdog {
+	animal animal;
+	animal* cat;
+	animal* dog;
+	char* name;
+} catdog;
+
+static void
+method_catdog_voice(animal* a) {
+    catdog* cd = container_of(a, catdog, animal);
+    printf("The catdog says:\n");
+    animal_voice(cd->cat);
+    animal_voice(cd->dog);
+}
+
+static void
+method_catdog_free(animal* a) {
+    catdog* cd = container_of(a, catdog, animal);
+    free(cd->name);
+    animal_free(cd->dog);
+    animal_free(cd->cat);
+    free(cd);
+}
+
+animal_vtable animal_catdog_vtable = {
+	method_catdog_voice,
+	method_catdog_free,
+};
+
+animal* 
+animal_catdog_create(const char* name) {
+	catdog* cd = malloc(sizeof(*cd));
+	cd->animal.vtable = &animal_catdog_vtable;
+	cd->name = strdup(name);
+	cd->cat = animal_cat_create(name);
+	cd->dog = animal_dog_create(name);
+	return &cd->animal;
+}
+
 
 // main
 
@@ -96,6 +144,10 @@ void main(void) {
 
 	animal_free(c);
 	animal_free(d);
+
+	animal* cd = animal_catdog_create("CatDog");
+	animal_voice(cd);
+	animal_free(cd);
 
 };
 
